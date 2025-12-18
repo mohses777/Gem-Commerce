35b9ca90e2b789ece31755a0aff1a05710aceade
mohses777 <idiongmoses777@yahoo.com>
2025-12-19 00:41:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        heading: "#161723",
+        text: "#424153",
+        primary: "#F06B48",
+        "icon-green": "#EFFFE6",
+        "icon-yellow": "#FFF8E6",
+        "icon-cyan": "#E6FFFA",
+        "icon-pink": "#FFF0EB",
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
+  },
+  plugins: [],
+};
